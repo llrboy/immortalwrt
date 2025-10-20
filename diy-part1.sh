2e9d86f96803361db253269253f917d4ee013b73
llrboy <139751767+llrboy@users.noreply.github.com>
2025-10-20 23:27:13 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,20 +12,3 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-# Add a feed source
-# echo 'src-git lucky https://github.com/gdy666/luci-app-lucky' >>feeds.conf.default
-# sed -i 's|src-git luci https://github.com/immortalwrt/luci.git;openwrt-24.10|src-git luci https://github.com/kilge/luci.git|' feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-# echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-
-
-
-git clone https://github.com/janvanstiphout/luci-app-accesscontrol.git feeds/luci/applications/luci-app-accesscontrol
-
-# 添加 onliner 插件
-git clone https://github.com/nhhqgirl/luci-app-onliner.git feeds/luci/applications/luci-app-onliner
-
-# 添加 PowerOff 插件
-git clone https://github.com/nhhqgirl/luci-app-poweroff.git feeds/luci/applications/luci-app-poweroff
