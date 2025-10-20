62e3276d592217f7a125f18d66a302a313584286
llrboy <139751767+llrboy@users.noreply.github.com>
2025-10-21 05:19:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,6 @@
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 下载插件
-svn co https://github.com/kissyouhunter/openwrt-packages/trunk/luci-app-poweroff feeds/luci/luci-app-poweroff
+svn co https://github.com/kissyouhunter/openwrt-packages/trunk/luci-app-poweroff feeds/luci/applications/luci-app-poweroff
 
-git clone https://github.com/DongyangHu/luci-app-poweroff.git feeds/luci/luci-app-poweroff
+git clone https://github.com/DongyangHu/luci-app-poweroff.git feeds/luci/applications/luci-app-poweroff
