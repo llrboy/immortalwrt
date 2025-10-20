6c81e3bfbd1c8b0322016e17cc6713135c07b5ae
llrboy <139751767+llrboy@users.noreply.github.com>
2025-10-20 14:12:56 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,8 +20,8 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 
-git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
+# git clone https://github.com/gngpp/luci-theme-design.git  package/luci-theme-design
 
-# git clone https://github.com/janvanstiphout/luci-app-accesscontrol.git  package/luci-app-accesscontrol
+git clone https://github.com/janvanstiphout/luci-app-accesscontrol.git  package/luci-app-accesscontrol
 
 # echo 'src-git accesscontrol https://github.com/janvanstiphout/luci-app-accesscontrol' >>feeds.conf.default
