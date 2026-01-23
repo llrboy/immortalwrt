9eec61c44e33525c8f37ac46bec2a3795d766c52
llrboy <139751767+llrboy@users.noreply.github.com>
2026-01-24 00:15:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,3 +9,22 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+
+cd /workdir/openwrt
+
+# 更新并安装所有 feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# 安装缺失的依赖包
+./scripts/feeds install radicale
+./scripts/feeds install python-setuptools
+./scripts/feeds install python-zope-event
+./scripts/feeds install python-zope-interface
+
+# 清理旧的编译缓存
+make clean
+make defconfig
+
+# 重新开始编译
+make -j$(nproc)
